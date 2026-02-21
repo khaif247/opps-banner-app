2eed1c8c500b2da1e07fdf564373df7243b1d249
khaif247 <ekdumperfect@gmail.com>
2026-02-21 22:30:49 +0530

    Added UC2 Print Banner assignment

---
+++ UC2.java
@@ -0,0 +1,14 @@
+public class UC2_PrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****    *****    *****    ***** ");
+        System.out.println(" *     *  *     *  *     *  *     *");
+        System.out.println(" *     *  *     *  *     *  *      ");
+        System.out.println(" *     *  *     *  * * * *   ***** ");
+        System.out.println(" *     *  *     *  *             * ");
+        System.out.println(" *     *  *     *  *        *    * ");
+        System.out.println("  *****    *****   *         ***** ");
+
+    }
+}
